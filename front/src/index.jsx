43b74c48dfdfb6e648c45fe757dc8558bde52ade
terandard <terandard0218@gmail.com>
2020-08-07 00:15:54 +0900

    ツールバーのUI追加

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -4,12 +4,14 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 
 import CalendarBoard from "./components/CalendarBoard/container";
+import Navigation from "./components/Navigation/container";
 import rootReducer from "./redux/rootReducer";
 
 const store = createStore(rootReducer);
 
 const App = () => (
     <Provider store={store}>
+        <Navigation />
         <CalendarBoard />
     </Provider>
 );
