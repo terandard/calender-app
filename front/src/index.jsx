1eb4194f9ee4367ab820d8220e9310bf21df0e35
terandard <terandard0218@gmail.com>
2020-08-10 14:55:34 +0900

    DatePicker追加

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import DayjsUtils from "@date-io/dayjs";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 
 import CalendarBoard from "./components/CalendarBoard/container";
 import Navigation from "./components/Navigation/container";
@@ -11,8 +13,10 @@ const store = createStore(rootReducer);
 
 const App = () => (
     <Provider store={store}>
-        <Navigation />
-        <CalendarBoard />
+        <MuiPickersUtilsProvider utils={DayjsUtils}>
+            <Navigation />
+            <CalendarBoard />
+        </MuiPickersUtilsProvider>
     </Provider>
 );
 
