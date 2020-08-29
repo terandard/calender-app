7568773b38b0d9e02314972c0b74d3bcfba07fe3
terandard <terandard0218@gmail.com>
2020-08-29 15:46:43 +0900

    通信のエラーハンドリングを行う

--- front/src/redux/schedules/effects.js
+++ front/src/redux/schedules/effects.js
@@ -1,6 +1,7 @@
 import {
   schedulesSetLoading, schedulesFetchItem,
-  schedulesAddItem, schedulesDeleteItem
+  schedulesAddItem, schedulesDeleteItem,
+  schedulesAsyncFailure
 } from "./actions";
 import { get, post, deleteRequest } from "../../services/api";
 import { formatSchedule } from "../../services/schedule";
@@ -8,29 +9,41 @@ import { formatSchedule } from "../../services/schedule";
 export const asyncSchedulesFetchItem = ({ month, year }) => async dispatch => {
   dispatch(schedulesSetLoading());
 
-  const result = await get(`schedules?month=${month}&year=${year}`);
+  try {
+    const result = await get(`schedules?month=${month}&year=${year}`);
 
-  const formatedSchedule = result.map(r => formatSchedule(r));
+    const formatedSchedule = result.map(r => formatSchedule(r));
 
-  dispatch(schedulesFetchItem(formatedSchedule));
+    dispatch(schedulesFetchItem(formatedSchedule));
+  } catch (err) {
+    dispatch(schedulesAsyncFailure(err.message));
+  }
 };
 
 export const asyncSchedulesAddItem = schedule => async dispatch => {
   dispatch(schedulesSetLoading());
 
-  const body = { ...schedule, date: schedule.date.toISOString() };
-  const result = await post("schedules", body);
+  try {
+    const body = { ...schedule, date: schedule.date.toISOString() };
+    const result = await post("schedules", body);
 
-  const newSchedule = formatSchedule(result);
-  dispatch(schedulesAddItem(newSchedule));
+    const newSchedule = formatSchedule(result);
+    dispatch(schedulesAddItem(newSchedule));
+  } catch (err) {
+    dispatch(schedulesAsyncFailure(err.message));
+  }
 };
 
 export const asyncSchedulesDeleteItem = id => async (dispatch, getState) => {
   dispatch(schedulesSetLoading());
   const currentSchedules = getState().schedules.items;
 
-  await deleteRequest(`schedules/${id}`);
-  // 成功したらローカルのstateを削除
-  const newSchedules = currentSchedules.filter(s => s.id !== id);
-  dispatch(schedulesDeleteItem(newSchedules));
+  try {
+    await deleteRequest(`schedules/${id}`);
+    // 成功したらローカルのstateを削除
+    const newSchedules = currentSchedules.filter(s => s.id !== id);
+    dispatch(schedulesDeleteItem(newSchedules));
+  } catch (err) {
+    dispatch(schedulesAsyncFailure(err.message));
+  }
 };
