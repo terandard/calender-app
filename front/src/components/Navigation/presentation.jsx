23fef3d7c941f472703af00dc3c36917cd43fda1
terandard <terandard0218@gmail.com>
2020-08-29 16:22:08 +0900

    ボタンのホバー表示追加

--- front/src/components/Navigation/presentation.jsx
+++ front/src/components/Navigation/presentation.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IconButton, Toolbar, Typography, withStyles } from "@material-ui/core";
+import { IconButton, Toolbar, Typography, withStyles, Tooltip } from "@material-ui/core";
 import ArrowBackIos from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIos from "@material-ui/icons/ArrowForwardIos";
 import DehazeIcon from "@material-ui/icons/Dehaze";
@@ -27,12 +27,16 @@ const Navigation = ({ setNextMonth, setPreviousMonth, setMonth, month }) => {
             <StyledTypography color="textSecondary" variant="h5" component="h1">
                 カレンダー
             </StyledTypography>
-            <IconButton size="small" onClick={setPreviousMonth}>
-                <ArrowBackIos />
-            </IconButton>
-            <IconButton size="small" onClick={setNextMonth}>
-                <ArrowForwardIos />
-            </IconButton>
+            <Tooltip title="前の月" placement="bottom">
+                <IconButton size="small" onClick={setPreviousMonth}>
+                    <ArrowBackIos />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="次の月" placement="bottom">
+                <IconButton size="small" onClick={setNextMonth}>
+                    <ArrowForwardIos />
+                </IconButton>
+            </Tooltip>
             <StyledDatePicker
                 value={month}
                 onChange={setMonth}
