ff106ac1f1badbaca22f06ef8ae40aade124ae0c
terandard <terandard0218@gmail.com>
2020-08-29 16:39:17 +0900

    dialogを閉じる時に確認する

--- front/src/components/AddScheduleDialog/container.jsx
+++ front/src/components/AddScheduleDialog/container.jsx
@@ -2,6 +2,7 @@ import AddScheduleDialog from "./presentation";
 import { connect } from "react-redux";
 import { addScheduleCloseDialog, addScheduleSetValue, addScheduleStartEdit } from "../../redux/addSchedule/actions";
 import { asyncSchedulesAddItem } from "../../redux/schedules/effects";
+import { isCloseDialog } from "../../services/schedule";
 
 const mapStateToProps = state => ({ schedule: state.addSchedule });
 
@@ -21,16 +22,24 @@ const mapDispatchToProps = dispatch => ({
     }
 });
 
-const mergeProps = (stateProps, dispatchProps) => ({
-    ...stateProps,
-    ...dispatchProps,
-    saveSchedule: () => {
-        const {
-            schedule: { form: schedule }
-        } = stateProps;
-        dispatchProps.saveSchedule(schedule);
-    }
-});
+const mergeProps = (stateProps, dispatchProps) => {
+    const {
+        schedule: { form: schedule }
+    } = stateProps;
+
+    return {
+        ...stateProps,
+        ...dispatchProps,
+        saveSchedule: () => {
+            dispatchProps.saveSchedule(schedule);
+        },
+        closeDialog: () => {
+            if (isCloseDialog(schedule)) {
+                dispatchProps.closeDialog();
+            }
+        }
+    };
+};
 
 export default connect(
     mapStateToProps,
